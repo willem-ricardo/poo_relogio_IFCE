dd28f266314626aa8e0ad716ff04a947077d2cfa
Willem de Lima Ricardo <wlricardo@alu.ufc.br>
2019-04-15 20:19:42 -0300

    Criação de métodos para exibição das horas

--- hora.py
+++ hora.py
@@ -10,6 +10,7 @@ class Hora:
             self.__minuto = 0
             self.__segundo = 0
         
+    ### Métodos de atribuição
     def set_hora(self, hora):
         if 0 <= hora <= 24:
             self.__hora = hora
@@ -21,7 +22,40 @@ class Hora:
     def set_segundo(self, segundo):
         if 0 <= segundo <= 59:
             self.__segundo = segundo
+            
+    ### Métodos de exibição
+    def get_hora(self):
+        return self.__hora
+    
+    def get_minuto(self):
+        return self.__minuto
+    
+    def get_segundo(self):
+        return self.__segundo
+    
+    def __str__(self):
+        return "{:02d}:{:02d}:{:02d}".format(
+            self.__hora,
+            self.__minuto,
+            self.__segundo)
+    
+    def str_12(self):
+        return "{:02d}:{:02d}:{:02d} {:s}".format(
+            12 if self.__hora == 0 or self.__hora == 12 else self.__hora % 12,
+            self.__minuto,
+            self.__segundo,
+            "AM" if self.__hora < 12 else "PM")
+    
+    ### Métodos utilitários
+    def mais_um_segundo(self):
+        self.__segundo += 1
         
-    def mostre_hora(self):
-        return "{:02d}:{:02d}:{:02d}".format(self.__hora, self.__minuto, self.__segundo)
-        
+        if self.__segundo == 60:
+            self.__segundo = 0
+            self.__minuto += 1
+            if self.__minuto == 60:
+                self.__minuto = 0
+                self.__hora += 1
+                if self.__hora == 24:
+                    self.__hora = 0     
+            
--- main_relogio.py
+++ main_relogio.py
@@ -2,7 +2,8 @@ import hora
 
 h1 = hora.Hora(12,15,30)
 h2 = hora.Hora(7, 16, 33)
-    
 
-print(h1.mostre_hora())
-print(h2.mostre_hora())
+
+for i in range(100):
+    print(h1, h1.str_12)
+    h1.mais_um_segundo()
