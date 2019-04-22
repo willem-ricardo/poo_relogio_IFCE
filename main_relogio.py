1c339ce0e4ade0e2c59bd435e5f3b61f7b1a3060
Willem de Lima Ricardo <wlricardo@gmail.com>
2019-04-22 09:34:31 -0300

    Exibindo a hora na label

--- main_relogio.py
+++ main_relogio.py
@@ -1,12 +1,26 @@
+import tkinter as tk
+from tkinter import ttk
 import hora
 
 h1 = hora.Hora(12,15,30)
-h2 = hora.Hora(7, 16, 33)
+#h2 = hora.Hora(7, 16, 33)
+#
+#print(h1)
+#print(h2)
+#print(50*'-')
+#h1.mais_um_segundo()
+#h2.mais_um_segundo()
+#print(h1)
+#print(h2)
 
-print(h1)
-print(h2)
-print(50*'-')
-h1.mais_um_segundo()
-h2.mais_um_segundo()
-print(h1)
-print(h2)
+main_window = tk.Tk()
+main_window.title('Relógio')
+
+lblHora = ttk.Label(main_window, text=h1.__str__())
+lblHora.grid(column=0, row=0, padx=5)
+#lblMinuto = ttk.Label(main_window, text=h1.get_minuto)
+#lblMinuto.grid(column=1, row=0, padx=5)
+#lblSegundo = ttk.Label(main_window, text=h1.get_segundo)
+#lblSegundo.grid(column=2, row=0, padx=5)
+
+main_window.mainloop()
---
+++ teste.py
@@ -0,0 +1,4 @@
+import hora
+
+h1 = hora.Hora(12, 44, 1)
+print(h1)
