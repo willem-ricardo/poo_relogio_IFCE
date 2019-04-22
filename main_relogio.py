7e9a361befb4ce4ff6d97f7e5313cf8f8a34d1a8
Willem de Lima Ricardo <wlricardo@gmail.com>
2019-04-22 11:37:25 -0300

    Inserindo a biblioteca datetime

--- main_relogio.py
+++ main_relogio.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+from datetime import datetime as dt
 import hora
 
-h1 = hora.Hora(12,15,30)
+#now = dt.now()
+#h1 = hora.Hora(now.hour, now.minute, now.second)
 #h2 = hora.Hora(7, 16, 33)
 #
 #print(h1)
@@ -12,15 +14,22 @@ h1 = hora.Hora(12,15,30)
 #h2.mais_um_segundo()
 #print(h1)
 #print(h2)
-
-main_window = tk.Tk()
-main_window.title('Relógio')
-
-lblHora = ttk.Label(main_window, text=h1.__str__())
-lblHora.grid(column=0, row=0, padx=5)
+#
+#main_window = tk.Tk()
+#main_window.title('Relógio')
+#
+#lblHora = ttk.Label(main_window, text=h1.__str__())
+#lblHora.grid(column=0, row=0, padx=5)
 #lblMinuto = ttk.Label(main_window, text=h1.get_minuto)
 #lblMinuto.grid(column=1, row=0, padx=5)
 #lblSegundo = ttk.Label(main_window, text=h1.get_segundo)
 #lblSegundo.grid(column=2, row=0, padx=5)
+
+main_window = tk.Tk()
+main_window.title('Relógio')
+now = dt.now()
+h1 = hora.Hora(now.hour, now.minute, now.second)
+lblHora = ttk.Label(main_window, text=h1.__str__())
+lblHora.grid(column=0, row=0, padx=5)
 
 main_window.mainloop()
